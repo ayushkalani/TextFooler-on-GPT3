146b18703069939e72b21022eef4822e40c27927
Ayush Kalani <kalani.ayush@gmail.com>
2022-12-04 21:41:09 -0700

    yelp running code 100

--- yelp-gpt3.py
+++ yelp-gpt3.py
@@ -1,24 +1,56 @@
 import os
 import csv
 import openai
+os.environ["OPENAI_API_KEY"] = "ayush"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+PREFIX = "This is yelp review. Classify wether it is a positive or a negative review. Positive is 2, and negative is 1."
+# 2 is positive, 1 is negative
 
 
 def read_csv():
-    with open("./bwq.csv", 'r') as file:
+    total_csv_count = 38_000
+    total_run_successfully = 0
+    correct = 0
+    with open("./data/yelp/test.csv", 'r') as file:
         csvreader = csv.reader(file)
         for row in csvreader:
-            print(row)
+            sentence = row[1]
+            label = row[0]
+            response = run_inference(sentence)
+            if 'choices' in response:
+                total_run_successfully += 1
+                test_label = response["choices"][0]["text"].strip('\n').lower()
+                if any(x in test_label for x in ['positive', '2']):
+                    if label == '2':
+                        correct += 1
+                elif any(x in test_label for x in ['negative', '1']):
+                    if label == '1':
+                        correct += 1
+                else:
+                    print("mismatch")
+            if total_run_successfully == 100:
+                break
+    print("total_run_successfully")
+    print(total_run_successfully)
+    print("correct")
+    print(correct)
 
 
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="This is yelp review. Classify wether it is a positive or a negative review. Positive is 2, and negative is 1.\n\nContrary to other reviews, I have zero complaints about the service or the prices. I have been getting tire service here for the past 5 years now, and compared to my experience with places like Pep Boys, these guys are experienced and know what they're doing. Also, this is one place that I do not feel like I am being taken advantage of, just because of my gender. Other auto mechanics have been notorious for capitalizing on my ignorance of cars, and have sucked my bank account dry. But here, my service and road coverage has all been well explained - and let up to me to decide. And they just renovated the waiting room. It looks a lot better than it did in previous years.",
-    temperature=0,
-    max_tokens=64,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0
-)
+def run_inference(text):
+    prompt = PREFIX + "\n\n" + text
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return response
 
-print(response)
+
+if __name__ == "__main__":
+    print("------YELP--------GPT3----------")
+    read_csv()
